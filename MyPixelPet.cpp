cc09b99321d32fbe3c973d68a1933f2877efef8b
Ricardo <00032226@uca.edu.sv>
2026-06-25 22:08:32 -0600

    creacion del proyecto y declaracion de los include que se van a ocupar a lo largo del proyecto

---
+++ MyPixelPet.cpp
@@ -0,0 +1,14 @@
+#include <iostream>  //cout, cin  -> entrada y salida en consola
+#include <fstream>   //ifstream, ofstream -> leer y escribir archivos
+#include <string>    //string, getline, to_string -> manejo de texto
+#include <ctime>     //time(), time_t, difftime() -> cooldowns y temporizador
+#include <cstdlib>   //system("cls") -> limpiar la pantalla
+#include <windows.h> //SetConsoleOutputCP, Sleep -> consola Windows
+
+using namespace std;
+
+int main (){
+
+    
+    return 0;
+}
