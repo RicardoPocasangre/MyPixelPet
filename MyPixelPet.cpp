43dc643cd66ae50c33748a74f8f7e22416c06251
Valeria Abigail Vásquez Martínez <00023826@uca.edu.sv>
2026-06-28 11:16:16 -0600

    Declaración de variables

--- MyPixelPet.cpp
+++ MyPixelPet.cpp
@@ -6,6 +6,24 @@
 #include <windows.h> //SetConsoleOutputCP, Sleep -> consola Windows
 
 using namespace std;
+const int TOTAL_ESTADISTICAS = 4;
+const int TOTAL_ESPECIES = 4;
+const int MAX_PARTIDAS = 5;
+const int VALOR_INICIAL = 50;
+const int LIMITE_MAXIMO = 100;
+const int LIMITE_MINIMO = 0;
+const int SEGUNDOS_COOLDOWN = 60;
+const int SEGUNDOS_RESCATE = 10;
+const int UMBRAL_ADVERTENCIA = 20;
+const int LARGO_BARRA = 10;
+const int RETARDO_ANIMACION = 12;
+
+struct Mascota {
+    string nombre;
+    int indiceEspecie;
+    int estadisticas[TOTAL_ESTADISTICAS];
+    time_t ultimaAccion[TOTAL_ESTADISTICAS];
+};
 
 int main (){
 
